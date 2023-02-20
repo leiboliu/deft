e79244ba7b5430d46c6ca3a6c662e55512894662
leiboliu <17636764+leiboliu@users.noreply.github.com>
2023-02-20 19:18:49 +1100

    update settings

--- deft/settings/production.py
+++ deft/settings/production.py
@@ -1,6 +1,4 @@
 from deft.settings.common import *
-import django_heroku
-django_heroku.settings(locals())
 # production settings to
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
